77c932dbf14a2d33eeb4350c51899a8d78de6ee9
nick <nschmoll@csumb.edu>
2018-02-07 19:14:22 +0000

    index.php from jasons space

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
-<!DOCTYPE php>
-<php>
-<!-- 
-main overview page
--->
-<head>
-    <meta charset="utf-8">
-    <title>general index</title>
-    <link href="CSS/styles.css" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <h1>Nick Schmoll</h1>
-    </header>
-    
-    <?php include_once("/labs/personal_portfolio/index.html"); ?>
-    <nav>
-        <hr width="50%" />
-        <a href="/assignments/HW1/index.html">HW1</a>
-        <a href="/labs/personal_portfolio/index.html">lab1</a>
-    </nav>
-
-</body>
-</php>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Nick Schmoll</title>
+    </head>
+    <body>
+        <div>
+            <h1>Labs</h1>
+            <a href="/labs/personal_portfoli">Lab 1</a>
+        </div>
+    </body>
+</html>
