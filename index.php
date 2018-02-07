ebcc14d62fe119f1a257f772b03a463051d59a8b
nick <nschmoll@csumb.edu>
2018-02-07 19:16:13 +0000

    again

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <div>
             <h1>Labs</h1>
-            <a href="/labs/personal_portfoli">Lab 1</a>
+            <a href="/labs/personal_portfolio">Lab 1</a>
         </div>
     </body>
 </html>
