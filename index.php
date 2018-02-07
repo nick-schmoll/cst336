51c4862e0dc4eb2dc108cf7327a5bdbeb307119a
nick <nschmoll@csumb.edu>
2018-02-07 18:52:38 +0000

    maybe now its fixed

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<!-- 
+main overview page
+-->
+<head>
+    <meta charset="utf-8">
+    <title>general index</title>
+    <link href="CSS/styles.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>Nick Schmoll</h1>
+    </header>
+    
+    <nav>
+        <hr width="50%" />
+        <a href="/assignments/HW1/index.html">HW1</a>
+        <a href="/labs/personal_portfolio/index.html">lab1</a>
+    </nav>
+    <br /><br />
+    <footer>
+        <hr>
+         <figure id="csumb_logo">
+            <img src="img/csumb_logo.png" alt="Picture of CSUMB logo" />
+        </figure>
+        cst238. 2018&copy; Schmoll <br />
+        for academic purposes only.
+    </footer>
+    
+</body>
+</html>
